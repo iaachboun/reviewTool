5d6dbc4ee93d0ca889f6eb5c261cb8a40f07f565
Bow <1993bow@gmail.com>
2018-11-23 11:02:30 +0100

    werkt nu, alleen nog null values weghalen uit database

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -24,21 +24,22 @@ class ReviewController extends Controller
     public function review(Request $request)
     {
         $data = $request->all();
-        $source = 'Robohost';
 
         $sources = array("Robohost","iXLhosting","HostingNet","Domeinwinkel","Hosting2Go","Mijndomein","SoHosted","Antagonist","Hostnet","Versio");
 
         $source = $sources[0];
 
-        for ($o = 0; $o < 10 ; $o++) {
+        for ($o = 0; $o < 9 ; $o++) {
 
-            for ($i = 0; $i <= 18 ; $i++) {
+            for ($i = 0; $i <= 17 ; $i++) {
                 var_dump($data[$source][$i]);
 
-                DB::table('review')->insert([
-                    ['review' => $data[$source][$i]],
+                DB::table('review')->insert(
+                    ['review' => $data[$source][$i]]
+                );
+                DB::table('review')->insert(
                     ['source' => $source]
-                ]);
+                );
             }
             $source = $sources[$o];
 
